cf14ea98587764d1af85cfc13e2f292d7d665b81
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-06-07 18:10:56 -0300

    Se refactorizo el codigo mejorando las funciones, se mejoro la modularizacion, se agregaron fechas dinamicas para un mejor registro visual en los gastos

--- logica_gastos.py
+++ logica_gastos.py
@@ -1,3 +1,5 @@
+import datetime
+
 # AGREGAR GASTO
 
 def agregar_gasto(historial, categoria, monto, descripcion):
@@ -21,7 +23,8 @@ def agregar_gasto(historial, categoria, monto, descripcion):
     nuevo_gasto = {
         "categoria" : categoria,
         "monto" : monto,
-        "descripcion" : descripcion
+        "descripcion" : descripcion,
+        "fecha" : datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
     }
     
     historial.append(nuevo_gasto)
@@ -64,3 +67,72 @@ def calcular_total(historial):
     
     total = sum(gasto['monto'] for gasto in historial)
     return f"El total de los gastos registrados es de: {total:,.2f}"
+
+# BUSQUEDA DE GASTOS
+
+def buscar_gasto(historial, termino):
+    
+    encontrado = False
+    
+    for busqueda in historial:
+        
+        if termino.lower() in busqueda['categoria'].lower() or termino.lower() in busqueda['descripcion'].lower():
+            print(f"Categoria: {busqueda['categoria']} | Descripción: {busqueda['descripcion']}")
+            encontrado = True
+            
+    if not encontrado:
+        print(f"No se encontraron coincidencias para su busqueda")
+        
+# ELIMINAR GASTOS
+
+def eliminar_gasto(historial):
+    for i, gasto in enumerate(historial, start=1):
+        print(f"{i}. {gasto['categoria']} - ${gasto['monto']}")
+        
+
+    solicitud_eliminar = int(input("Indique el numero del registro que desea eliminar: "))
+
+    if solicitud_eliminar < 1 or solicitud_eliminar > len(historial):
+        print("No se encontraron registros a su solicitud")
+    else:
+        eliminado = historial.pop(solicitud_eliminar - 1)
+        print(f"Registro de '{eliminado['categoria']}' eliminado con éxito.")
+        
+# EDITAR GASTOS
+
+def editar_gasto(historial):
+    for i, gasto in enumerate(historial, start=1):
+        print(f"{i}. {gasto['categoria']} - ${gasto['monto']}")
+        
+    solicitud_editar = int(input("Indique el numero del registro que desea editar: "))
+    
+    if 1 <= solicitud_editar <= len(historial):
+        print(f"El registro que busca existe!")
+        
+        nueva_categoria = input("Indique la nueva categoria: ")
+        nuevo_monto = int(input("Indique el nuevo monto: "))
+        nueva_descripcion = input("Indique la nueva descripción: ")
+        
+        historial[solicitud_editar - 1].update({'categoria':nueva_categoria, 'monto':nuevo_monto, 'descripcion':nueva_descripcion})
+        print(f"Datos reeplazados satisfactoriamente")
+        print(f"Sus nuevos datos son: {historial[solicitud_editar - 1]}")
+    else:
+        print("El registro que busca no extiste!")
+        
+# REPORTE DE GASTOS
+
+def mostrar_reporte(historial):
+    if not historial:
+        return 
+    
+    montos = [gasto['monto'] for gasto in historial]
+    total = sum(montos)
+    promedio = total / len(montos)
+    maximo = max(montos)
+    minimo = min(montos)
+
+    print(f"--- REPORTE ESTADÍSTICO ---")
+    print(f"Total gastado:      ${total:,.2f}")
+    print(f"Promedio de gastos: ${promedio:.2f}")
+    print(f"Gasto más elevado:  ${maximo:.2f}")
+    print(f"Gasto más bajo:     ${minimo:.2f}")
